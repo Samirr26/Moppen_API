029c544d63009c49afbd3dd4270f3cdceee78284
Samir <samir.rademakers2@gmail.com>
2023-06-08 22:09:56 +0200

    Created tests

---
+++ Tests/JokesControllerTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moppen_API.Controllers;
+using Moppen_API.DataContext;
+using Moppen_API.Models;
+using NSubstitute;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moppen_API_Tests
+{
+    [TestClass]
+    public class JokesControllerTests
+    {
+        private JokesController _jokesController;
+
+        public JokesControllerTests()
+        {
+            _jokesController = new JokesController(Substitute.For<IConfiguration>());
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _jokesController = new JokesController(Substitute.For<IConfiguration>());
+        }
+
+        [TestMethod]
+        public async Task Get_Random_Joke_Test_Should_Not_Be_Null()
+        {
+            var response = _jokesController.getRandomJoke();
+            response.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public async Task Get_Random_Joke_Test_Should_Be_Type_Of_Joke()
+        {
+            var response = _jokesController.getRandomJoke();
+            response.Should().BeOfType<Task<Joke>>();
+        }
+
+        [TestMethod]
+        public async Task Get_Joke_Based_Author_Test_Should_Not_Be_Null()
+        {
+
+            var response = _jokesController.getJokesBasedOnAuthor("Samir");
+            response.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public async Task Get_Joke_Based_Author_Test_Should_Be_Type_Of_List()
+        {
+
+            var response = _jokesController.getJokesBasedOnAuthor("Samir");
+            response.Should().BeOfType<Task<IEnumerable<String>>>();
+        }
+
+        [TestMethod]
+        public async Task Get_Joke_Based_Subject_Test_Should_Not_Be_Null()
+        {
+            var response = _jokesController.getJokesBasedOnAuthor("IT");
+            response.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public async Task Get_Joke_Based_Subject_Test_Should_Be_Type_Of_List()
+        {
+            var response = _jokesController.getJokesBasedOnAuthor("IT");
+            response.Should().BeOfType<Task<IEnumerable<String>>>();
+        }
+    }
+}
---
+++ Tests/TestData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moppen_API_Tests
+{
+    public class TestData
+    {
+        public static string Author = "Samir";
+        public static string Subject = "IT";
+        public static string FullJoke = "Why did the programmer need glasses? Because he could not C#";
+    }
+}
